290f2a9f700d6a8bee1a7a5cbc8a23df9ffdee76
Tej Pratap <bankebihari5463@gmail.com>
2024-09-15 21:07:43 +0530

    user controller setup begin

--- app/controller/user.controller.js
+++ app/controller/user.controller.js
@@ -1,11 +1,13 @@
 const db = require('../model');
 const sequelize = db.sequelize;
 const { QueryTypes, Utils } = require("sequelize");
+const User = db.user;
+
 
 
 const getAllUsers = async (req, res) => {
     try {
-        const users = await db.User.findAll({
+        const users = await User.findAll({
             where: {
                 id: 2
             },
@@ -36,7 +38,7 @@ const createUser = async (req, res) => {
         const { firstName, lastName } = req.body;
 
         //creating a user first type
-        const newUser = await db.User.build({
+        const newUser = await User.build({
             firstName,
             lastName
         });
@@ -45,7 +47,7 @@ const createUser = async (req, res) => {
         console.log("new user", newUser);
 
         // Create a new user second type
-        const jane = await db.User.create({ firstName, lastName });
+        const jane = await User.create({ firstName, lastName });
         console.log("Jane's auto-generated ID:", jane.id);
 
         return res.status(200).json({
@@ -65,7 +67,7 @@ const createUser = async (req, res) => {
 const updateUser = async (req, res) => {
     try {
         console.log(req.params)
-        const updatedUser = await db.User.update({ lastName: "Doe" }, {
+        const updatedUser = await User.update({ lastName: "Doe" }, {
             where: {
                 id: req.params.userid
             }
@@ -87,7 +89,7 @@ const updateUser = async (req, res) => {
 
 const deleteUser = async (req, res) => {
     try {
-        const deletedUser = await db.User.destroy({
+        const deletedUser = await User.destroy({
             where: {
               id: req.params.userid
             }
@@ -107,62 +109,6 @@ const deleteUser = async (req, res) => {
     }
 }
 
-
-//raw query
-const updateCase = async (req) => {
-    let today, query, where, params = [];
-    try {
-        today = dt.getCurrentDateTime();
-        params.push(req.body.cl_claim_status);
-        params.push(req.body.cl_claim_sub_status);
-        params.push(today);
-        params.push(req.decoded.id);
-        params.push(req.id);
-        query = "UPDATE " + c_table + " SET cl_claim_status = ?, cl_claim_sub_status = ?, modified = ?, modified_by = ? ";
-        where = "WHERE id = ? ";
-        req.db = {
-            query: query,
-            where: where,
-            params: params
-        };
-        return await this.updateQuery(req);
-    } catch (err) {
-        return logger.dumpError(err);
-    }
-};
-
-const updateQuery = async (req) => {
-    let query, params, where, result;
-    query = req.db.query;
-    params = req.db.params;
-    where = req.db.where;
-    if (where)
-        query += " " + where
-    query += " LIMIT 1 ";
-    const connection = await mysql.connection();
-    try {
-        result = await connection.query(query, params);
-        if (result && result.changedRows === 1) {
-            return req.response = {
-                responseCode: 200,
-                responseMessage: "Successfully done.",
-                responseData: result
-            }
-        } else {
-            return req.response = {
-                responseCode: 0,
-                responseMessage: "Unable to update data due to an error.",
-                responseData: result
-            }
-        }
-
-    } catch (err) {
-        return logger.dumpDBError(err);
-        // throw err;
-    } finally {
-        await connection.release();
-    }
-}
 
 
 
